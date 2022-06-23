6d061aaeb39cc867b7f43be798d36e0a92c3cc93
genrihromanov <setskeys1332@gmail.com>
2022-06-24 00:18:25 +0300

    test com

--- main.py
+++ main.py
@@ -26,5 +26,3 @@ for v in soup.find_all('element'):
         elif key == 'type':
             elementType = value
             print(f'{key} = {elementType}')
-            print(f'{key} = {elementType}')
-            print(f'{key} = {elementType}')
