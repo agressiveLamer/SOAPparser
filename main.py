7148019c4b11c2ef8f1a8e956bc1efe5db650b5c
genrihromanov <setskeys1332@gmail.com>
2022-06-24 16:25:24 +0300

    coment logick

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 import mimetypes
 
 from bs4 import BeautifulSoup
+import csv
 from lxml.builder import unicode
 import re
 
 with open('f/2.xhtml') as f:
     soup = BeautifulSoup(f, 'lxml-xml')
 
-for v in soup.find_all('element'):
-    attrs = v.attrs  # присваиваем словарь с атрибутами тега
-    for key in attrs:
-        value = attrs[key]  # значение атрибута
-        if key == 'name':
-            name = value
-            print(f'{key} = {name}')
-        elif key == 'minOccurs':
-            minOccurs = value
-            print(f'{key} = {minOccurs}')
-        elif key == 'maxOccurs':
-            maxOccurs = value
-            print(f'{key} = {maxOccurs}')
-        elif key == 'id':
-            elementID = value
-            print(f'{key} = {elementID}')
-        elif key == 'type':
-            elementType = value
-            print(f'{key} = {elementType}')
+
+def parser(XMLfile):
+    for v in soup.find_all('element'):
+        attrs = v.attrs  # присваиваем словарь с атрибутами и значениями тега
+        for key in attrs:
+            value = attrs[key]  # значение атрибута
+            yield key, value
+        """ if key == 'name':
+             name = value
+             print(f'{key} = {name}')
+             return name
+         elif key == 'minOccurs':
+             minOccurs = value
+             print(f'{key} = {minOccurs}')
+             return minOccurs
+         elif key == 'maxOccurs':
+             maxOccurs = value
+             print(f'{key} = {maxOccurs}')
+             return maxOccurs
+         elif key == 'id':
+             elementID = value
+             print(f'{key} = {elementID}')
+             return elementID
+         elif key == 'type':
+             elementType = value
+             print(f'{key} = {elementType}')
+             return elementType"""
+
+
+for value, key in parser(soup):
+    print(f'{value} = {key}')
