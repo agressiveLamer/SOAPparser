9ecbd51c3a2302deeb574b67d7efdee51734961b
genrihromanov <setskeys1332@gmail.com>
2022-06-24 16:32:17 +0300

    """ Этот кусок кода не работает             if key == 'name':                 name = value                 print(f'{key} = {name}')             elif key == 'minOccurs':                 minOccurs = value                 print(f'{key} = {minOccurs}')             elif key == 'maxOccurs':                 maxOccurs = value                 print(f'{key} = {maxOccurs}')             elif key == 'id':                 elementID = value                 print(f'{key} = {elementID}')             elif key == 'type':                 elementType = value                 print(f'{key} = {elementType}')                 """

--- main.py
+++ main.py
@@ -15,27 +15,22 @@ def parser(XMLfile):
         for key in attrs:
             value = attrs[key]  # значение атрибута
             yield key, value
-        """ if key == 'name':
-             name = value
-             print(f'{key} = {name}')
-             return name
-         elif key == 'minOccurs':
-             minOccurs = value
-             print(f'{key} = {minOccurs}')
-             return minOccurs
-         elif key == 'maxOccurs':
-             maxOccurs = value
-             print(f'{key} = {maxOccurs}')
-             return maxOccurs
-         elif key == 'id':
-             elementID = value
-             print(f'{key} = {elementID}')
-             return elementID
-         elif key == 'type':
-             elementType = value
-             print(f'{key} = {elementType}')
-             return elementType"""
 
+            """ Этот кусок кода не работает
+            if key == 'name':
+                name = value
+                print(f'{key} = {name}')
+            elif key == 'minOccurs':
+                minOccurs = value
+                print(f'{key} = {minOccurs}')
+            elif key == 'maxOccurs':
+                maxOccurs = value
+                print(f'{key} = {maxOccurs}')
+            elif key == 'id':
+                elementID = value
+                print(f'{key} = {elementID}')
+            elif key == 'type':
+                elementType = value
+                print(f'{key} = {elementType}')
+                """
 
-for value, key in parser(soup):
-    print(f'{value} = {key}')
