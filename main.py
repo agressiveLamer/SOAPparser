c549577eb77530dc752d2f4ca4336d24f49420e7
genrihromanov <setskeys1332@gmail.com>
2022-06-26 23:47:30 +0300

    добавил дополнительные условия

--- main.py
+++ main.py
@@ -14,15 +14,35 @@ with open('f/result.csv', 'w') as result:
         ['name', 'type', 'minOccurs', 'maxOccurs', 'id']
     )
 
-BaseListOfElements = []
 for v in soup.find_all('element'):
     attrs = v.attrs  # присваиваем словарь с атрибутами и значениями тега
-    with open('f/result.csv', 'a') as result:
-        writer = csv.writer(result)
-        writer.writerow(
-            [attrs.get('name'),
-             attrs.get('type'),
-             attrs.get('minOccurs'),
-             attrs.get('maxOccurs'),
-             attrs.get('id')]
-        )
+    if attrs.get('maxOccurs') is None:
+        with open('f/result.csv', 'a') as result:
+            writer = csv.writer(result)
+            writer.writerow(
+                [attrs.get('name'),
+                 attrs.get('type'),
+                 attrs.get('minOccurs'),
+                 '1',
+                 attrs.get('id')]
+            )
+    elif attrs.get('minOccurs') is None:
+        with open('f/result.csv', 'a') as result:
+            writer = csv.writer(result)
+            writer.writerow(
+                [attrs.get('name'),
+                 attrs.get('type'),
+                 '1',
+                 attrs.get('maxOccurs'),
+                 attrs.get('id')]
+            )
+    else:
+        with open('f/result.csv', 'a') as result:
+            writer = csv.writer(result)
+            writer.writerow(
+                [attrs.get('name'),
+                 attrs.get('type'),
+                 attrs.get('minOccurs'),
+                 attrs.get('maxOccurs'),
+                 attrs.get('id')]
+            )
