11d982add01ac76fcf871c105913fabd29faa748
genrihromanov <setskeys1332@gmail.com>
2022-06-26 19:23:24 +0300

    парсер технически работает, в ячейки все забивается

--- main.py
+++ main.py
@@ -8,30 +8,21 @@ import re
 with open('f/2.xhtml') as f:
     soup = BeautifulSoup(f, 'lxml-xml')
 
+with open('f/result.csv', 'w') as result:
+    writer = csv.writer(result)
+    writer.writerow(
+        ['name', 'type', 'minOccurs', 'maxOccurs', 'id']
+    )
 
-def parser(XMLfile):
-    keys = []
-    for v in soup.find_all('element'):
-        attrs = v.attrs  # присваиваем словарь с атрибутами и значениями тега
-        for key in attrs:
-            value = attrs[key]  # значение атрибутов
-            keys = keys.append(key) #не работает..
-            with open('f/result.csv', mode='a') as result:
-                writer = csv.writer(result)
-                writer.writerow(
-                    (key)
-                )
-            """if key == 'name':
-                name = value
-            elif key == 'minOccurs':
-                minOccurs = value
-            elif key == 'maxOccurs':
-                maxOccurs = value
-            elif key == 'id':
-                elementID = value
-            elif key == 'type':
-                elementType = value
-            """
-
-
-print(parser(soup))
+BaseListOfElements = []
+for v in soup.find_all('element'):
+    attrs = v.attrs  # присваиваем словарь с атрибутами и значениями тега
+    with open('f/result.csv', 'a') as result:
+        writer = csv.writer(result)
+        writer.writerow(
+            [attrs.get('name'),
+             attrs.get('type'),
+             attrs.get('minOccurs'),
+             attrs.get('maxOccurs'),
+             attrs.get('id')]
+        )
